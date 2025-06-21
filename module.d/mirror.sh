9ca10e4942a31b769d5db70a190c8e92a858d3ad
syoch <syoch64@gmail.com>
2025-06-21 23:21:57 +0900

    Update: Changed repository

--- module.d/mirror.sh
+++ module.d/mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "" > $PREFIX/etc/apt/sources.list
-echo "deb https://mirrors.krnk.org/apt/termux/termux-main stable main" >> $PREFIX/etc/apt/sources.list
-echo "deb https://mirrors.krnk.org/apt/termux/termux-root root stable" >> $PREFIX/etc/apt/sources.list
-echo "deb https://mirrors.krnk.org/apt/termux/termux-x11 x11 main" >> $PREFIX/etc/apt/sources.list
+echo "deb https://mirrors.cbrx.io/apt/termux/termux-main stable main" >> $PREFIX/etc/apt/sources.list
+echo "deb https://mirrors.cbrx.io/apt/termux/termux-root root stable" >> $PREFIX/etc/apt/sources.list
+echo "deb https://mirrors.cbrx.io/apt/termux/termux-x11 x11 main" >> $PREFIX/etc/apt/sources.list
